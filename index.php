d44d2b3527e0ea1b2cefff3dcbed894f2372e048
JnckCode <54052722+jnckcode@users.noreply.github.com>
2020-04-13 14:15:40 +0700

    Updating bug fix

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 		<div class="container">
 			<p>Hello world, this site was build for instagram rest-api</p>
 			</div>
+<form name="cdn" action="igvid.php" method="post">
+<input type="url" name="url" placeholder="Input URL"/>
+<input type="submit" value="Go !" />
+</form>
 			</body>
 </html>
