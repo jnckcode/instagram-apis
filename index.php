dd07ed6bffca387ce74851934bf255c7935b93ed
JnckCode <54052722+jnckcode@users.noreply.github.com>
2020-04-13 14:02:51 +0700

    rest-api
    fixed all bugs.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	<title>Instagram|Rest-Api</title>
+	</head>
+	
+	<body>
+		<div class="container">
+			<p>Hello world, this site was build for instagram rest-api</p>
+			</div>
+			</body>
+</html>
